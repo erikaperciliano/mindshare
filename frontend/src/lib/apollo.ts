f4ce15595230a146f0dabeb7b6f2532f845e7ebd
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-13 15:18:53 -0300

    feat: addd Apollo Client configuration

---
+++ frontend/src/lib/apollo.ts
@@ -0,0 +1,10 @@
+import { ApolloClient, HttpLink, ApolloLink, InMemoryCache } from "@apollo/client"
+
+const httpLink = new HttpLink({
+    uri: "http://localhost:4000/graphql"
+})
+
+export const apolloClient = new ApolloClient({
+    link: ApolloLink.from([httpLink]),
+    cache: new InMemoryCache()
+})
