355c7272fc1c7e8ddb0acad2346c797e84d13d46
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-13 17:23:33 -0300

    feat: add toaster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,23 @@
 import { Layout } from '@/components/Layout'
 import { Routes, Route } from "react-router-dom"
 import { Login } from '@/components/pages/Login'
-import { SignUp } from './components/pages/SignUp'
+import { Toaster } from './components/ui/sonner'
+import { SignUp } from './components/pages/Signup'
 
 function App() {
 
   return (
-    <Layout>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={ <SignUp/>} />
-      </Routes>
-    </Layout>
+    <>
+      <Layout>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={ <SignUp/>} />
+        </Routes>
+      </Layout>
+
+      <Toaster/>
+    </>
+  
   )
 }
 
