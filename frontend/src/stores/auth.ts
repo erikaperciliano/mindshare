d5abb502b09e721c06cc999e88a00c0adec019ef
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-13 17:28:34 -0300

    feat(auth): implement authentication store

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,6 +1,7 @@
 import { apolloClient } from "@/lib/apollo";
+import { LOGIN } from "@/lib/graphql/mutations/login";
 import { REGISTER } from "@/lib/graphql/mutations/register";
-import type { RegisterInput, User } from "@/types";
+import type { LoginInput, RegisterInput, User } from "@/types";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
@@ -12,11 +13,21 @@ type RegisterMutationData = {
   };
 };
 
+type LoginMutationData = {
+  login: {
+    token: string;
+    refreshToken: string;
+    user: User;
+  };
+};
+
+
 interface AuthState {
   user: User | null;
   token: string | null;
   isAuthenticated: boolean;
   signup: (data: RegisterInput) => Promise<boolean>;
+  login: (data: LoginInput) => Promise<boolean>;
 }
 
 export const useAuthStore = create<AuthState>()(
@@ -62,6 +73,45 @@ export const useAuthStore = create<AuthState>()(
             
         } catch (error) {
             console.log("Erro ao fazer o cadastro")
+            throw error
+          }
+      },
+
+      login: async (loginData: LoginInput) => {
+        try {
+            const { data } = await apolloClient.mutate<LoginMutationData, { data: LoginInput }>({
+                mutation: LOGIN,
+                variables: {
+                    data: {
+                        email: loginData.email,
+                        password: loginData.password
+                    }
+                }
+            })
+            
+            if (data?.login) {
+                const { token, user } = data.login
+
+                set({
+                    user: {
+                        id: user.id,
+                        name: user.name,
+                        email: user.email,
+                        role: user.role,
+                        createdAt: user.createdAt,
+                        updatedAt: user.updatedAt
+                    },
+                    token,
+                    isAuthenticated: true
+                })
+
+                return true
+            }
+
+            return false
+            
+        } catch (error) {
+            console.log("Erro ao fazer o login")
             throw error
           }
       },
