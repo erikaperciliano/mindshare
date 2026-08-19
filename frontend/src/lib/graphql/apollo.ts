5a198b44fb12bdc90a32c97699e657ef652382f7
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-18 22:42:18 -0300

    chore: configure production api

--- frontend/src/lib/graphql/apollo.ts
+++ frontend/src/lib/graphql/apollo.ts
@@ -3,7 +3,7 @@ import { ApolloClient, HttpLink, ApolloLink, InMemoryCache } from "@apollo/clien
 import { SetContextLink } from '@apollo/client/link/context'
 
 const httpLink = new HttpLink({
-    uri: "http://localhost:4000/graphql"
+    uri: import.meta.env.VITE_API_URL
 })
 
 const authLink = new SetContextLink((prevContext) => {
