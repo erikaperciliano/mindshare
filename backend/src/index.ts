f49e02ecd45c24cf647f67f0732ddc4943dd7b34
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-08-18 23:30:59 -0300

    fix: configure cors for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
     app.use(
         cors({
-            origin: "http://localhost:5173",
+            origin: process.env.FRONTEND_URL,
             credentials: true,
         })
     );
